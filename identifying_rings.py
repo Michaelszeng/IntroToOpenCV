698e5fa86cf4304e60a4224f8ddc09ec4b7966a5
Michaelszeng <michael.zeng.22@bishops.com>
2020-09-22 20:52:38 -0700

    improved organization and documentation, attempted Hough line detection

--- identifying_rings.py
+++ identifying_rings.py
@@ -11,10 +11,11 @@ def main():
     The Goal is, given images with a stack of 0, 1, or 4, orange rings, the program should be able
     to detect how many rings are in the stack. I attempt to detect using multiple methods, the results of which are printed at the end.
     """
+    #Initalize the variables to hold the results
     avgHueResult = -1
     cntAspectRatioResults = -1
 
-
+    #Get the path to the image of the rings
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required = True, help = "Path to the image")
     args = vars(ap.parse_args())
@@ -32,13 +33,13 @@ def main():
         obstacles = False
 
 
-    image = cv2.imread(args["image"])
+    image = cv2.imread(args["image"])   #Get the image from the argument
     image = imutils.resize(image, newWidth = 720)   #Ensure the image is 720 width so the crop will work
     # cv2.imshow("Original", image)
     original = image.copy()
 
-    image = imutils.crop(image, (300, 220), (450, 330))   #Crop image to remove background noise
-    cv2.imshow("Cropped", image)
+    image = imutils.crop(image, (300, 220), (450, 330))   #Crop image to remove unecessary background noise
+    cv2.imshow("Original Image Cropped", image)
 
 
 
@@ -46,36 +47,24 @@ def main():
     Detection Method 1: Isolate the ring stack, create a contour around it, then detect
     the height to width ratio of the contour. This seems to be the most robust.
     """
-    #Turn all non-orange pixels black.
-    # ringsOnlyImg = removeBackground(image)
-    # cv2.imshow("Rings", ringsOnlyImg)
-    #
-    # #Turning into Greyscale and Blurring to prepare for edge detection and contouring
-    # blurred = cv2.cvtColor(ringsOnlyImg, cv2.COLOR_BGR2GRAY)
-    # blurred = cv2.GaussianBlur(blurred, (5, 5), 0)
-    # cv2.imshow("Blurred", blurred)
-    #
-    # #apply edge detection
-    # edged = cv2.Canny(blurred, 100, 400)   #2 threshold values have to be very high to ignore the gaps between rings, and irgnore other imperfections in the bg
-    # cv2.imshow("Edged", edged)
-    edged = getEdges(image)
+    edged = getEdges(image)     #Get image with black background and edges
 
     #get the contours
-    #cnts is a list of the actual contours
-    (cnts, useless) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (cnts, useless) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    #cnts is a list of the actual contours
     cv2.drawContours(image, cnts, -1, (255, 120, 0), 2)   #You can only draw one contour at a time, or all of them
-    cv2.imshow("Image with Contours", image)
+    cv2.imshow("Contour Around Rings", image)
 
     for (i, c) in enumerate(cnts):
         (x, y, w, h) = cv2.boundingRect(c)  #returns tuple with rectangle dimensions to bound the contour c
         obj = image[y:y + h, x:x + w]   #cropping the image
-        cv2.imshow("Object #{}".format(i), obj)
 
+        #Based on the ration of width to height of the contour of the rings, determine the height of the stack
         if w > (2*h):
             cntAspectRatioResults = 1
         else:
             cntAspectRatioResults = 4
 
+    #If there are no contours, there are 0 rings
     if len(cnts) == 0:
         cntAspectRatioResults = 0
 
@@ -89,21 +78,22 @@ def main():
     Detection Method 2: find the average hue in a cropped image of the stack, and compare them
     to threshold values
     """
-    hueTotal = 0
     imageCropped = imutils.crop(original, (340, 270), (420, 320))   #Crop image to remove background noise
     if obstacles:
         imageCropped = imutils.crop(original, (320, 250), (400, 300))   #Crop image to remove background noise
 
-    imageCroppedIsolated = removeBackground(imageCropped)
-    imageHSVCroppedIsolated = cv2.cvtColor(imageCroppedIsolated, cv2.COLOR_BGR2HSV)
+    imageCroppedIsolated = removeBackground(imageCropped)   #color the background black to remove noise
+    imageHSVCroppedIsolated = cv2.cvtColor(imageCroppedIsolated, cv2.COLOR_BGR2HSV) #Converting to HSV
     cv2.imshow("imageHSVCroppedIsolated", imageHSVCroppedIsolated)
 
+    hueTotal = 0
     for r in range(0, imageHSVCroppedIsolated.shape[1] - 1):    #Loop through the width of the img
         for c in range(0, imageHSVCroppedIsolated.shape[0] - 1):    #Loop through the height of the img
-            (hue, sat, val) = imageHSVCroppedIsolated[c, r]
-            hueTotal = hueTotal + hue
-    hueAvg = hueTotal / (imageHSVCroppedIsolated.shape[1] * imageHSVCroppedIsolated.shape[0])
+            (hue, sat, val) = imageHSVCroppedIsolated[c, r]     #Get HSV values of the pixel
+            hueTotal = hueTotal + hue   #Append to the total hue value
+    hueAvg = hueTotal / (imageHSVCroppedIsolated.shape[1] * imageHSVCroppedIsolated.shape[0])   #Calculate the average hue
     # print("avg Hue: %d" % hueAvg)
+    #Based on the hue, determine the heigh of the ring stack based on pre-deterined thesholds
     if hueAvg > 3:
         avgHueResult = 4
     elif hueAvg > 0:
@@ -116,12 +106,23 @@ def main():
 
 
     """
-    Detection Method 3:
+    Detection Method 3: Using Hough Line Detection to detect the horizontal and vertical lines
+    of the ring stack. Based on the ration of the lenghts of these lines, we can determine
+    the size of the ring stack.
+
+    Unfortunately, this method wasn't finished in time.
     """
     # edged = getEdges(image)
-    # cv2.imshow("iamg: ", image)
-    # lines = cv2.HoughLinesP(edged, 1, np.pi/180, 80, minLineLength = 80, maxLineGap = 20)
-    # print(len(lines))
+    # cv2.imshow("edged: ", image)
+    # lines = cv2.HoughLinesP(edged, 1, np.pi/180, 40, minLineLength = 50, maxLineGap = 40)
+    # print(lines)
+    #
+    # for line in lines:  #Draw the lines found
+    #     x1, y1, x2, y2 = line[0]    #Get endpoints of line
+    #     cv2.line(edged, (x1, y1), (x2, y2), (0, 255, 0), 2)  #Draw Line
+    #
+    # cv2.imshow("lines", edged)
+    #
     # cv2.waitKey(0)
 
 
@@ -130,14 +131,14 @@ def removeBackground(image):
     Purpose: Turns all non-orange pixels to black, removes all the noise to improve
     accuracy and consistency.
     """
-    ringsOnlyImg = image.copy()
-    imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    ringsOnlyImg = image.copy()     #Don't want to edit the original image
+    imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)   #Convert to HSV to do hue detection
     for r in range(0, imageHSV.shape[1] - 1):    #Loop through the width of the img
         for c in range(0, imageHSV.shape[0] - 1):    #Loop through the height of the img
-            (hue, sat, val) = imageHSV[c, r]
-            if hue < 10 or hue > 18 or sat < 80:
-                ringsOnlyImg[c, r] = (0, 0, 0)
-    return ringsOnlyImg
+            (hue, sat, val) = imageHSV[c, r]    #Get the HSV values of current pixel
+            if hue < 10 or hue > 18 or sat < 80:    #If the hue value is not orange:
+                ringsOnlyImg[c, r] = (0, 0, 0)      #Set it to black
+    return ringsOnlyImg     #Return image with black background
 
 def getEdges(image):
     """
